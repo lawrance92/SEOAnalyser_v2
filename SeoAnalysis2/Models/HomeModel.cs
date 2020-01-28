6177eb6f3ee1dc8eaae710595c46aaf2a811bcbc
lawrance <lawrancekumar.work@gmail.com>
2020-01-28 20:37:05 +0800

    first commit for seo v2
    clear

---
+++ SeoAnalysis2/Models/HomeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeoAnalysis2.Models
+{
+    public class HomeModel
+    {
+        // Home model for store the information and pare to view to process
+        [Required]
+        public string URL { get; set; } // this field is required and cannot be empty
+        public bool Exist { get; set; } // Assign boolean value for website existing or not
+        public List<string> words { get; set; } // Assign the list of stop-words which is match with websites content
+    }
+}
